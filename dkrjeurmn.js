0f238ee451844d2520ca9623c4357ace8cb816fd
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2025-02-25 15:23:01 +0900

    Add files via upload

---
+++ dkrjeurmn.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	var vds = document.getElementById('vdsss');
+    
+    if (vds) {
+        vds.src = "https://vod.plaync.com/lineageM/new/1_castlerooping_PC_low.mp4";
+        document.getElementById('vdss').load();
+    }
+});
+
